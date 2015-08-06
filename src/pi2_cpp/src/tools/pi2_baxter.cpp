2d4c37449e97cf50b776a5a7c1815a6d5cd71e38
Zhen Zeng <zengzhen@umich.edu>
2015-08-06 18:18:14 -0400

    add pi2_baxter.cpp: prototype position control through c++ instead of Python

---
+++ src/pi2_cpp/src/tools/pi2_baxter.cpp
@@ -0,0 +1,33 @@
+/**
+ * \file        pi2_baxter.cpp
+ * \author      Zhen Zeng (zengzhen@umich.edu) 
+ */
+
+#include "pi2.h"
+#include <iostream>
+#include <ctime>
+#include "ros/ros.h"
+#include "baxter_core_msgs/JointCommand.h"
+
+int main(int argc, char** argv)
+{
+	//publish a sample joint position to pi2_baxter
+	ros::init(argc, argv, "pi2_baxter");
+	ros::NodeHandle n;
+	
+	ros::Publisher joint_position_pub = n.advertise<baxter_core_msgs::JointCommand>("robot/limb/left/joint_command", 1000);
+	ros::Rate loop_rate(1);
+	
+	while(ros::ok())
+	{
+		baxter_core_msgs::JointCommand msg;
+		msg.mode = baxter_core_msgs::JointCommand::POSITION_MODE;
+		msg.names = {"time","left_s0","left_s1","left_e0","left_e1","left_w0","left_w1"}; //,"left_w2","left_gripper"};
+		msg.command = {7.01,0.810955272759,-0.889685757539,-0.201837847861,1.91495434991,2.93148422081,-0.563894595801}; //,-1.44300820632,100.0};
+		
+		joint_position_pub.publish(msg);
+	}
+	
+	return 0;
+}
+
